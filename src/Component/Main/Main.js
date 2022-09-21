85192f3de1a89c376cd6c177bf352b41325ac686
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-20 20:36:35 -0700

    filter functionrefactored but still broken

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useCountries } from '../../hooks/useCountries';
 import CountryCard from '../CountryCard/CountryCard';
+import Filter from '../Filter/Filter';
 
 export default function Main() {
-  const countries = useCountries();
+  const { filterCountries } = useCountries();
   return (
     <div>
-      {countries.map((country) => {
-        return <CountryCard key={country.name} {...country} />;
-      })}
+      <Filter />
+      {filterCountries().map((name) => (
+        <CountryCard key={name.id} {...name} />
+      ))}
     </div>
   );
 }
